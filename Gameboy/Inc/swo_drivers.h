80cf087de7313ddd7792ed4eea0f17e182092dfb
SeanDauch <seandauch1@gmail.com>
2026-05-16 21:29:24 -0700

    moved SWO drivers to seprate proj

--- Gameboy/Inc/swo_drivers.h
+++
@@ -1,8 +0,0 @@
-#ifndef _swo_drivers_h
-    #define _swo_drivers_h
-
-    void swo_init_printf();
-
-    int __io_putchar(int ch);
-
-#endif
--- Gameboy/Src/syscall.c
+++ Gameboy/Src/syscall.c
@@ -30,9 +30,6 @@
 #include <sys/time.h>
 #include <sys/times.h>
 
-//* provides definition for __io_putchar
-#include "swo_drivers.h"
-
 
 /* Variables */
 extern int __io_putchar(int ch) __attribute__((weak));
